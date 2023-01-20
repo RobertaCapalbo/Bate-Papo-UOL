9f9f14edfb11ab08f4e89399a684db994d6400d0
Roberta Capalbo <robertacapalbo@hotmail.com>
2023-01-20 20:53:52 -0300

    Versão Apenas com Layout

---
+++ scipt.js
@@ -0,0 +1,9 @@
+function entrar(){
+    const inicio = document.querySelector('.tudo .escondido');
+    inicio.classlist.remove('escondido');
+}
+
+function abrir(){
+    const inicio = document.querySelector('.pessoas .escondido');
+    inicio.classlist.remove('escondido');
+}
